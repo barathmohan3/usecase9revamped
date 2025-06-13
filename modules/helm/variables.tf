16417c86efa378645c33f27eee4661d77efbb6ae
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-13 15:17:22 +0530

    Update variables.tf

--- modules/helm/variables.tf
+++ modules/helm/variables.tf
@@ -1,5 +1,14 @@
-variable "cluster_id" {}
-variable "lbc_iam_depends_on" {}
-variable "lbc_iam_role_arn" {}
-variable "vpc_id" {}
-variable "aws_region" {}
+variable "cluster_id" {
+}
+variable "cluster_endpoint" {
+}
+variable "cluster_certificate_authority_data" {
+}
+variable "lbc_iam_depends_on" {
+}
+variable "lbc_iam_role_arn" {
+}
+variable "vpc_id" {
+}
+variable "aws_region" {
+}
