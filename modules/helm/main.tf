6bbb0da9eb624e7b0a83094f6c454949f38d880a
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-13 15:17:01 +0530

    Update main.tf

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -1,49 +1,42 @@
-# ------------------------
-# Get EKS Cluster Details
-# ------------------------
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_id
-}
-
+# Datasource: EKS Cluster Auth 
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_id
 }
 
-# ------------------------
+
 # Kubernetes Provider
-# ------------------------
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-# ------------------------
 # HELM Provider
-# ------------------------
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = var.cluster_endpoint
+    cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
-# ------------------------
-# AWS Load Balancer Controller
-# ------------------------
+# Install AWS Load Balancer Controller using HELM
+
+# Resource: Helm Release 
 resource "helm_release" "loadbalancer_controller" {
-  depends_on = [var.lbc_iam_depends_on]
+  depends_on = [var.lbc_iam_depends_on]            
   name       = "aws-load-balancer-controller"
 
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
 
+  namespace = "kube-system"     
+
+  # Value changes based on your Region (Below is for us-east-1)
   set {
-    name  = "image.repository"
-    value = "602401143452.dkr.ecr.${var.aws_region}.amazonaws.com/amazon/aws-load-balancer-controller"
-  }
+    name = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/amazon/aws-load-balancer-controller" 
+  }       
 
   set {
     name  = "serviceAccount.create"
@@ -57,37 +50,35 @@ resource "helm_release" "loadbalancer_controller" {
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = var.lbc_iam_role_arn
+    value = "${var.lbc_iam_role_arn}"
   }
 
   set {
     name  = "vpcId"
-    value = var.vpc_id
-  }
+    value = "${var.vpc_id}"
+  }  
 
   set {
     name  = "region"
-    value = var.aws_region
-  }
+    value = "${var.aws_region}"
+  }    
 
   set {
     name  = "clusterName"
-    value = var.cluster_id
-  }
+    value = "${var.cluster_id}"
+  }    
+    
 }
 
-# ------------------------
-# Namespace for Monitoring
-# ------------------------
+
+# Create Namespace for grafana and prometheus
 resource "kubernetes_namespace" "monitoring" {
   metadata {
     name = "monitoring"
   }
 }
 
-# ------------------------
-# Prometheus & Grafana Stack
-# ------------------------
+# Helm to install and setup prometheus and grafana 
 resource "helm_release" "prometheus_grafana_stack" {
   name       = "kube-prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
@@ -95,7 +86,8 @@ resource "helm_release" "prometheus_grafana_stack" {
   version    = "58.0.0"
   namespace  = kubernetes_namespace.monitoring.metadata[0].name
   timeout    = 600
-
+  
+  # Retry mechanism
   max_history     = 5
   cleanup_on_fail = true
   wait            = true
@@ -122,7 +114,7 @@ resource "helm_release" "prometheus_grafana_stack" {
           selector:
             matchLabels:
               app.kubernetes.io/name: aws-load-balancer-controller
-
+    
     grafana:
       enabled: true
       adminPassword: "admin"
