7e9798063af37de019d1a2f8ab99d9029869b213
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-13 12:04:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-  terraform {
+terraform {
   backend "s3" {
-    bucket = "terraform-remote-st"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1" 
- 
-    # For State Locking
-    dynamodb_table = "terraform-locks"    
-  } 
-  } 
+    bucket         = "my-oidc-bucket-15328069840"
+    key            = "two-tier-app/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = false
+    dynamodb_table = "bmware-table"
+  }
+}
